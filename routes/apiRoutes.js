02fd66ecf5e0cca23e635edd8960c0ab7f74e099
Ryan Durso <rdurso86@gmail.com>
2020-03-15 23:05:04 -0400

    i did not install some of the modules properly and i was throwing errors

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+const db = require("../db/db.json");
+let newId = require("uuid")
+module.exports = function(app) {
+  
+  app.get("/api/notes", function(req, res) {
+    res.json(db);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    
+    let userNote = req.body;
+    let noteId = newId;
+    userNote.id = noteId;
+
+    db.push(userNote);
+
+    res.json(db);
+    
+  });
+  
+  app.delete("/api/notes", function(req, res) {
+    
+    userNote = [];
+    
+    res.json(db);
+
+  });
+
+
+
+}
